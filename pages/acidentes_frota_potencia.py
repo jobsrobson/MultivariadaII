a22fcedacb41f3bf66b0dfcab38084e105080f59
Marley Abe <52432980+marleyabe@users.noreply.github.com>
2025-11-27 12:45:53 -0300

    feat: add 2 pages

---
+++ pages/acidentes_frota_potencia.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# --- 1. CONFIGURAÇÃO DA PÁGINA ---
+st.set_page_config(
+    page_title="Frotas Mais Potentes Geram Mais Acidentes",
+    page_icon="2️⃣",
+    layout="wide"
+)
+st.title("Frotas Mais Potentes Geram Mais Acidentes")
